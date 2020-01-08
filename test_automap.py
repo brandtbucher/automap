edc13edab9fe638b7b633a5936409f1b8c4e716a
Brandt Bucher <brandtbucher@gmail.com>
2020-01-08 07:12:37 -0800

    Work around hypothesis bug.

--- test_automap.py
+++ test_automap.py
@@ -1,27 +1,29 @@
 from datetime import date, datetime
 from timeit import Timer, timeit
-from typing import Hashable, Iterable, Set, Type, TypeVar, FrozenSet, Tuple
+from typing import Hashable, Iterable, Set, Type, TypeVar, FrozenSet, Tuple, Union
 
 from hypothesis import assume, given, infer, seed, settings
 from hypothesis.strategies import from_type, frozensets, integers, sampled_from
 from pytest import mark, raises
 
-from automap import AutoMap
+from automap import AutoMap, FrozenAutoMap
 
 
-Atom = TypeVar("Atom", bytes, complex, date, datetime, float, int, str)
-CompositeAtom = TypeVar("CompositeAtom", Atom, Tuple[Atom, ...], FrozenSet[Atom])
+# Atom should just be Hashable, but hypothesis chokes on NaNs sometimes, so we
+# need to exclude float and complex... :(
+Atom = Union[bool, int, None, str, bytes, date, datetime]
+CompositeAtom = Union[Atom, Tuple[Atom, ...], FrozenSet[Atom]]
 
-keys = frozensets(from_type(CompositeAtom))
 
-
-@given(keys=keys)
-def test_auto_map___len__(keys: FrozenSet[Hashable]):
+@given(keys=infer)
+def test_auto_map___len__(keys: FrozenSet[CompositeAtom]):
     assert len(AutoMap(keys)) == len(keys)
 
 
-@given(keys=keys, others=keys)
-def test_auto_map___contains__(keys: FrozenSet[Hashable], others: FrozenSet[Hashable]):
+@given(keys=infer, others=infer)
+def test_auto_map___contains__(
+    keys: FrozenSet[CompositeAtom], others: FrozenSet[CompositeAtom]
+):
     a = AutoMap(keys)
     for key in keys:
         assert key in a
@@ -30,8 +32,10 @@ def test_auto_map___contains__(keys: FrozenSet[Hashable], others: FrozenSet[Hash
         assert key not in a
 
 
-@given(keys=keys, others=keys)
-def test_auto_map___getitem__(keys: FrozenSet[Hashable], others: FrozenSet[Hashable]):
+@given(keys=infer, others=infer)
+def test_auto_map___getitem__(
+    keys: FrozenSet[CompositeAtom], others: FrozenSet[CompositeAtom]
+):
     a = AutoMap(keys)
     for index, key in enumerate(keys):
         assert a[key] == index
@@ -41,18 +45,27 @@ def test_auto_map___getitem__(keys: FrozenSet[Hashable], others: FrozenSet[Hasha
             a[key]
 
 
-# @given(keys=keys)
-# def test_auto_map___hash__(keys: FrozenSet[Hashable]):
-#     assert hash(AutoMap(keys)) == hash(AutoMap(keys))
+@given(keys=infer)
+def test_auto_map___hash__(keys: FrozenSet[CompositeAtom]):
+    assert hash(FrozenAutoMap(keys)) == hash(FrozenAutoMap(keys))
 
 
-@given(keys=keys)
-def test_auto_map___iter__(keys: FrozenSet[Hashable]):
+@given(keys=infer)
+def test_auto_map___iter__(keys: FrozenSet[CompositeAtom]):
     assert [*AutoMap(keys)] == [*keys]
 
 
-@given(keys=keys, key=from_type(CompositeAtom))
-def test_issue_3(keys: FrozenSet[Hashable], key: Hashable):
+@given(keys=infer)
+def test_auto_map_add(keys: FrozenSet[CompositeAtom]):
+    a = AutoMap()
+    for l, key in enumerate(keys):
+        assert a.add(key) is None
+        assert len(a) == l + 1
+        assert a[key] == l
+
+
+@given(keys=infer, key=infer)
+def test_issue_3(keys: FrozenSet[CompositeAtom], key: CompositeAtom):
     assume(key not in keys)
     a = AutoMap(keys)
     a |= (key,)
