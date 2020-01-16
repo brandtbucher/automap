c1a7a45140924019d8042e00e80ef42d44298ca7
Brandt Bucher <brandtbucher@gmail.com>
2020-01-16 07:47:18 -0800

    Move performance.py to tasks.py.

--- tasks.py
+++ tasks.py
@@ -1,14 +1,22 @@
+from functools import reduce
 from glob import glob
+from itertools import product
+from multiprocessing import Pool
+from operator import mul
 from os import environ, remove, replace
+from random import random
 from subprocess import run
-from sys import executable, platform
+from sys import executable, getsizeof, platform
+from timeit import Timer
 
 from invoke import task
 
 
 class MockContext:
     @staticmethod
-    def run(command: str, env=None, replace_env=False) -> None:
+    def run(command: str, echo=False, env=None, replace_env=False) -> None:
+        if echo:
+            print(command)
         if env is not None:
             env = {**({} if replace_env else environ), **env}
         run(command, env=env, shell=True, check=True)
@@ -17,17 +25,17 @@ class MockContext:
 @task
 def clean(context):
     context = MockContext()
-    context.run(f"{executable} setup.py develop --uninstall")
+    context.run(f"{executable} setup.py develop --uninstall", echo=True)
     for artifact in ("*.egg-info", "*.so", "build", "dist"):
-        context.run(f"rm -rf {artifact}")
-    context.run("black .")
+        context.run(f"rm -rf {artifact}", echo=True)
+    context.run("black .", echo=True)
 
 
 @task(clean)
 def build(context):
     context = MockContext()
-    context.run(f"{executable} -m pip install --upgrade pip")
-    context.run(f"pip install -r requirements.txt")
+    context.run(f"{executable} -m pip install --upgrade pip", echo=True)
+    context.run(f"pip install -r requirements.txt", echo=True)
     context.run(
         f"{executable} setup.py develop sdist bdist_wheel",
         env={"CPPFLAGS": "-Werror -Wno-deprecated-declarations"},
@@ -42,7 +50,7 @@ def build(context):
         # This will remove the wheel if it was unchanged... but that will cause
         # our assert to fail later, which is what we want!
         for wheel in WHEELS:
-            context.run(f"auditwheel repair {wheel} -w dist")
+            context.run(f"auditwheel repair {wheel} -w dist", echo=True)
             remove(wheel)
     elif platform == "darwin":
         # We lie here, and say our 10.9 64-bit build is a 10.6 32/64-bit one.
@@ -61,24 +69,75 @@ def build(context):
     print("After:", *FIXED, sep="\n - ")
     assert len(WHEELS) == len(FIXED), "We gained or lost a wheel!"
 
-    context.run("twine check dist/*")
+    context.run("twine check dist/*", echo=True)
     for dist in [".", *glob("dist/*.tar.gz"), *glob("dist/*.whl")]:
-        context.run(f"pip install --force-reinstall --no-cache-dir {dist}")
+        context.run(f"pip install --force-reinstall --no-cache-dir {dist}", echo=True)
 
 
 @task(build)
 def test(context):
     context = MockContext()
-    context.run("pytest -v")
+    context.run("pytest -v", echo=True)
+
+
+def do_work(info):
+    from automap import FrozenAutoMap
+
+    namespace = {"FrozenAutoMap": FrozenAutoMap}
+    create_a = Timer("FrozenAutoMap(keys)", globals=namespace)
+    create_d = Timer("{k: i for i, k in enumerate(keys)}", globals=namespace)
+    access_a = Timer(
+        "for key in a: a[key]", "a = FrozenAutoMap(keys)", globals=namespace
+    )
+    access_d = Timer(
+        "for key in d: d[key]",
+        "d = {k: i for i, k in enumerate(keys)}",
+        globals=namespace,
+    )
+    kind, power, factor = info
+    items = factor * 10 ** power
+    namespace["keys"] = [*{kind(random()) for _ in range(items)}]
+    iterations = max(create_a.autorange()[0], create_d.autorange()[0])
+    create = create_a.timeit(iterations) / create_d.timeit(iterations)
+    size = getsizeof(FrozenAutoMap(namespace["keys"])) / getsizeof(
+        {k: i for i, k in enumerate(namespace["keys"])}
+    )
+    iterations = max(access_a.autorange()[0], access_d.autorange()[0])
+    access = access_a.timeit(iterations) / access_d.timeit(iterations)
+    return items, create, access, size
 
 
 @task(test)
 def performance(context):
     context = MockContext()
-    context.run(f"{executable} performance.py")
+
+    print("TYPE\tITEMS\tCREATE\tACCESS\tSIZE")
+
+    def geometric_mean(xs):
+        return reduce(mul, xs) ** (1 / len(xs))
+
+    with Pool() as pool:
+        for kind in (str,):
+            total_create = []
+            total_access = []
+            total_size = []
+            for items, create, access, size in pool.imap(
+                do_work, product((kind,), range(6), range(1, 10))
+            ):
+                print(
+                    f"{kind.__name__}\t{items:,}\t{create-1:+.0%}\t{access-1:+.0%}\t{size-1:+.0%}",
+                    flush=True,
+                )
+                total_create.append(create)
+                total_access.append(access)
+                total_size.append(size)
+            print(
+                f"{kind.__name__}\tMEAN\t{geometric_mean(total_create)-1:+.0%}\t{geometric_mean(total_access)-1:+.0%}\t{geometric_mean(total_size)-1:+.0%}",
+                flush=True,
+            )
 
 
 @task(test)
 def release(context):
     context = MockContext()
-    context.run("twine upload --skip-existing dist/*")
+    context.run("twine upload --skip-existing dist/*", echo=True)
