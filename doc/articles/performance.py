4800183571280062f9eac128f943871d106fb652
Christopher Ariza <ariza@flexatone.com>
2023-03-22 09:49:39 -0700

    updated performance.py

--- doc/articles/performance.py
+++ doc/articles/performance.py
@@ -399,7 +399,7 @@ CLS_FF = (
 FF_ORDER = [f.NAME for f in sorted(CLS_FF, key=lambda ff: ff.SORT)]
 
 # -------------------------------------------------------------------------------
-NUMBER = 1
+NUMBER = 50
 
 from itertools import product
 
